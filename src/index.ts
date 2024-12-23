70875271fd3f92976368c6f6a44f339c4a80111e
simplr-sh <simplr.sh@gmail.com>
2024-12-23 20:22:36 +0530

    Update redirect URL to point to API usage section in README

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = new Hono()
 
 app.get('/', (c) => {
   return c.redirect(
-    'https://github.com/simplr-sh/sw-api?tab=readme-ov-file#readme',
+    'https://github.com/simplr-sh/sw-api?tab=readme-ov-file#api-usage',
     302
   )
 })
