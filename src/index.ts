d7e34e5fdaea5f4f3c28f2027a4b735cab88d341
simplr-sh <simplr.sh@gmail.com>
2024-12-23 20:20:28 +0530

    Update redirect URL to point directly to the README section on GitHub

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.redirect('https://github.com/simplr-sh/sw-api', 302)
+  return c.redirect(
+    'https://github.com/simplr-sh/sw-api?tab=readme-ov-file#readme',
+    302
+  )
 })
 
 export default app
