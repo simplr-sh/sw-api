2ba84f3754f64f8b5cc39ed69a4f57f35613fcb6
simplr-sh <simplr.sh@gmail.com>
2024-12-23 20:55:30 +0530

    Simplify redirect URL to point directly to the GitHub repository

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.redirect(
-    'https://github.com/simplr-sh/sw-api?tab=readme-ov-file#api-usage',
-    302
-  )
+  return c.redirect('https://github.com/simplr-sh/sw-api', 302)
 })
 
 export default app
